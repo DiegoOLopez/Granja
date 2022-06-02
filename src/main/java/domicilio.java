01cee948b4c8dad21838d66faae6f37b009d1481
AlanHerCas <103047514+AlanHerCas@users.noreply.github.com>
2022-06-02 10:04:31 -0500

    domicilio interface
    Se crearon los métodos en interface

---
+++ src/main/java/domicilio.java
@@ -0,0 +1,24 @@
+public interface domicilio {
+
+    //String pais, estado, municipio, localidad, calle;
+
+     String getPais();
+
+     void setPais(String pais);
+
+     String getEstado();
+
+     void setEstado(String estado);
+
+     String getMunicipio();
+
+     void setMunicipio(String municipio);
+
+     String getLocalidad();
+
+     void setLocalidad(String localidad);
+
+     String getCalle();
+
+     void setCalle(String calle);
+}
